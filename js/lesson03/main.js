067bc1df3481f7b37a54c60419044fde6154115e
Alexander Kupsha <red2@mail.ru>
2021-02-03 11:00:08 +0200

    04, 5 пункт

--- js/lesson03/main.js
+++ js/lesson03/main.js
@@ -1,27 +1,39 @@
 'use strict';
 
-let money = +prompt('Ваш месячный доход?');
+let money = +prompt('Ваш месячный доход?', 55000);
 let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую:');
 let deposit = prompt('Есть ли у вас депозит в банке? Да / нет');
 let expenses1 = prompt('Введите обязательную статью расходов?');
+let amount1 = +prompt('Во сколько это обойдется?', 25000);
 let expenses2 = prompt('Введите обязательную статью расходов?');
-let amount1 = +prompt('Во сколько это обойдется?');
-let amount2 = +prompt('Во сколько это обойдется?');
+let amount2 = +prompt('Во сколько это обойдется?', 10000);
 
 let income = 'фриланс';
 let mission = 200000;
 let period = 6;
+let accumulatedMonth = getAccumulatedMonth();
+let monthLeft = getTargetMonth();
 
 
 let lowerCase = addExpenses.toLocaleLowerCase();
 let result = lowerCase.split(', ');
 
-let budgetMonth = (money - (amount1 + amount2));
-let budgetDay = (budgetMonth / 30);
+let budgetDay = (accumulatedMonth / 30);
 
-let monthLeft = (mission - (period * budgetMonth)) / budgetMonth;
 
 deposit = deposit === "Да" || deposit === 'да' ? true : false;
+
+function getExpensesMonth() {
+  return (amount1 + amount2);
+};
+
+function getAccumulatedMonth() {
+  return money - getExpensesMonth();
+};
+
+function getTargetMonth() {
+  return (mission - (period * accumulatedMonth)) / accumulatedMonth;
+}
 
 if ( budgetDay > 1200 ) {
   console.log('У вас высокий уровень дохода');
@@ -33,24 +45,6 @@ if ( budgetDay > 1200 ) {
   console.log('Что то пошло не так');
 }
 
-// Саш, нужен твой совет так правильно делать, если через switch? Не разу им не пользовался решил попробовать. У знакомого видел просто в Реакте что он использовал switch case.
-
-/*
-switch (true) {
-  case budgetDay > 1200:
-    console.log('У вас высокий уровень дохода');
-    break;
-  case budgetDay > 600 && budgetDay < 1200:
-    console.log('У вас средний уровень дохода');
-    break;
-    case budgetDay < 600 &&  budgetDay > 0:
-      console.log('К сожалению у вас уровень дохода ниже среднего');
-      break;
-    case budgetDay > -1:
-      console.log('Что то пошло не так');
-      break;
-}
-*/
 
 console.log(typeof money);
 console.log(typeof income); 
@@ -62,10 +56,10 @@ console.log(result);
 console.log(`Период равен ${period} месяцев.`);
 console.log(`Цель заработать ${mission} рублей/долларов/гривен/юани`);
 console.log('budgetDay: ', Math.floor(budgetDay));
-console.log('budgetMonth: ', Math.ceil(budgetMonth));
+console.log('budgetMonth: ', getAccumulatedMonth());
 console.log('Бюджет на месяц:', money);
 console.log('expenses1: ', expenses1);
-console.log('expenses2: ', expenses2);
 console.log('amount1: ', amount1);
+console.log('expenses2: ', expenses2);
 console.log('amount2: ', amount2);
 console.log('Сколько месяцев осталось до цели:', Math.ceil(monthLeft));
