bc6b0af43c55ae688862932251c8b1adcc7aae92
Alexander Kupsha <red2@mail.ru>
2021-03-01 17:42:53 +0200

    in progress

--- js/main.js
+++ js/main.js
@@ -1,3 +1,31 @@
+const wrapper = document.querySelector('.wrapper');
+
 class DomElement {
 
+  constructor(selector, height, width, bg, fontSize) {
+    this.selector = selector;
+    this.height = height;
+    this.width = width;
+    this.bg = bg;
+    this.fontSize = fontSize;
+  }
+
+  addElement(selector) {
+    const addDivElement = document.createElement('div');
+    const addParagraphElement = document.createElement('p');
+
+    if (selector.indexOf('.', 1)) {
+      wrapper.append(addDivElement);
+      addDivElement.setAttribute('class', 'addCLassDivElement');
+    } else if (selector.indexOf('#', 1)) {
+      wrapper.append(addParagraphElement);
+      addParagraphElement.setAttribute('id', 'addParagraphIdElement');
+    }
+  }
 };
+
+const domElement = new DomElement();
+// - если строка selector начинается с точки, создаем div с классом
+// - если строка selector  начинается с решетки # то создаем параграф с id
+
+console.log(domElement.addElement('.'));
