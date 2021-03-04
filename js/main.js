57f90fc47a481190016f4005344d64aeb98bcd4f
Alexander Kupsha <red2@mail.ru>
2021-03-04 11:55:53 +0200

    finished

---
+++ js/main.js
@@ -0,0 +1,23 @@
+'use strict'
+
+class First {
+  hello() {
+    console.log('Привет я метод родителя!');
+  }
+}
+
+class Second extends First {
+  constructor(hello) {
+    super(hello);
+  }
+
+  hello() {
+    console.log('А я наследуемый метод!');
+  }
+}
+
+const first = new First();
+const second = new Second();
+
+first.hello();
+second.hello();
