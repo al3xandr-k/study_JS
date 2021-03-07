40c631bcf4e42ca28784977af3eec4ed39ffcdc3
Alexander Kupsha <red2@mail.ru>
2021-03-07 19:56:31 +0200

    finished lesson17_a

--- js/homeWork/main.js
+++ js/homeWork/main.js
@@ -1,26 +1,26 @@
 'use strict'
 
-const a = document.querySelector('#a');
-const b = document.querySelector('#b');
-const btnSum = document.querySelector('#sum');
-const btnMult = document.querySelector('#mult');
-const res = document.querySelector('#res');
-let result;
+const a = document.querySelector('#a')
+const b = document.querySelector('#b')
+const btnSum = document.querySelector('#sum')
+const btnMult = document.querySelector('#mult')
+const res = document.querySelector('#res')
+let result
 
 const calculator = {
   sum() {
-    result = +a.value + +b.value;
+    result = +a.value + +b.value
 
-    console.log('result: ', result);
+    console.log('result: ', result)
 
-    calculator.show();
+    calculator.show()
   },
   mult() {
-    result = +a.value * +b.value;
+    result = +a.value * +b.value
 
-    console.log('result: ', result);
+    console.log('result: ', result)
 
-    calculator.show();
+    calculator.show()
   },
   show() {
     res.value = result
@@ -28,12 +28,12 @@ const calculator = {
 }
 
 btnSum.addEventListener('click', ()=>{
-  calculator.sum();
+  calculator.sum()
   calculator.show()
-});
+})
 
 btnMult.addEventListener('click', ()=>{
-  calculator.mult();
+  calculator.mult()
   calculator.show()
-});
+})
 
