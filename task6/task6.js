18cbee7bc1af3b43eaefca45e13c90ec2e44a3f4
Alexander Kupsha <red2@mail.ru>
2021-03-11 18:15:30 +0200

    finished

--- task6/task6.js
+++ task6/task6.js
@@ -1,60 +1,60 @@
-window.addEventListener('DOMContentLoaded', () => {
+let print1 = function() {
+  console.log('Крот');
+}
+let print2 = function() {
+  console.log('овце,');
+}
+let print3 = function() {
+  console.log('жирафу,');
+}
+let print4 = function() {
+  console.log('зайке');
+}
+let print5 = function() {
+  console.log('голубые');
+}
+let print6 = function() {
+  console.log('сшил');
+}
+let print7 = function() {
+  console.log('фуфайки');
+}
 
-  const outText1 = document.querySelector('#out-text1');
-  const outText2 = document.querySelector('#out-text2');
-  const outText3 = document.querySelector('#out-text3');
-  const outText4 = document.querySelector('#out-text4');
+let func1 = function() {
+  // ...
+  print3()
+};
 
-  setInterval(() => {
-    const date = new Date();
-    const hours = date.getHours();
-    const minutes = date.getMinutes();
-    const seconds = date.getSeconds();
+let func2 = function() {
+  func1();
 
-    const checkTime = time => {
-      if (time < 10) {
-        time = '0' + time;
-      }
-      return time;
-
-    };
-    
-    outText2.textContent = 'Текущее время: ' + checkTime(hours) + ':' + checkTime(minutes) + ':' + checkTime(seconds) + ' PM';
-
-    if (hours >= '05' && hours <= '11') {
-      outText1.textContent = 'Доброе утро.';
-    } else if (hours >= '12' && hours <= '15') {
-      outText1.textContent = 'Добрый день.';
-    } else if (hours >= '16' && hours <= '23') {
-      outText1.textContent = 'Добрый вечер.';
-    } else if (hours >= '24' && hours <= '04') {
-      outText1.textContent = 'Доброй ночи.';
-    }
+  setTimeout(function() {
+    // ...
+    print6()
   }, 1000);
+}
 
-  const currentDay = () => {
+let func3 = function() {
+  setTimeout(function() {
+    func2();
+    // ...
+    print4()
+  }, 250);
 
-    const arrDays = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
+  // ...
+  print2()
+}
 
-    const date = new Date();
-    const day = date.getDay();
+setTimeout(function() {
+  // ...
+  print5()
+  setTimeout(function() {
+    // ...
+    print7()
+  }, 750);
+}, 500);
 
-    arrDays.forEach((item, index) => {
-      if (day === index) {
-        outText3.textContent = 'Сегодня: ' + item;
-      }
-    });
-  };
+// ...
+print1()
 
-  const daysLeft = (deadline) => {
-    const date = new Date(deadline).getTime();
-    const today = new Date().getTime();
-    const daysLeft = (date - today) / 1000;
-    const day = Math.floor(daysLeft / 60 / 60 / 24);
-
-    outText4.textContent = 'До нового года осталось ' + day + ' дней.';
-  };
-
-  daysLeft('2021-12-31');
-  currentDay();
-});
+func3();
