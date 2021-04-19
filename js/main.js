6315b4c554c5eb9afd9c49f0bfb2e3f92bc87167
Alexander Kupsha <red2@mail.ru>
2021-04-19 19:14:45 +0300

    in progress

--- js/main.js
+++ js/main.js
@@ -26,32 +26,125 @@ const modalCallBackForm = () => {
 
 modalCallBackForm()
 
-const smoothScroll = () => {
+// const smoothScroll = () => {
 
-  //btn top menu
-  const servicesBtn = document.querySelectorAll('a[href="#services"]')
-  const faqBtn = document.querySelectorAll('a[href="#faq"]')
-  const contactsBtn = document.querySelectorAll('a[href="#contacts"]')
+//   //btn top menu
+//   const servicesBtn = document.querySelector('a[href="#services"]')
+//   const faqBtn = document.querySelector('a[href="#faq"]')
+//   const contactsBtn = document.querySelector('a[href="#contacts"]')
 
-  //all sections
-  const services = document.querySelector('#services')
-  const faq = document.querySelector('#faq')
-  const contacts = document.querySelector('#contacts')
+//   //all sections
+//   const services = document.getElementById('services')
+//   const faq = document.getElementById('faq')
+//   const contacts = document.getElementById('contacts')
 
-  console.log(contactsBtn, servicesBtn, faqBtn);
+//   //Menu button smooth scroll to next section.
 
-  //Menu button smooth scroll to next section.
-  const allSection = (button, section) => {
-    button.addEventListener('click', (event) => {
-      event.preventDefault()
+//   const topMenu = document.querySelector('.top-menu')
 
-      section.scrollIntoView({ behavior: 'smooth' })
-    })
+//   topMenu.addEventListener('click', event => {
+//     const target = event.target
+//     event.preventDefault()
+//     if (target === servicesBtn) {
+
+//       services.scrollIntoView({ behavior: "smooth" });
+//     }
+
+//   })
+
+// const allSection = (button, section) => {
+//   button.addEventListener('click', (event) => {
+//     event.preventDefault()
+
+//     section.scrollIntoView({ behavior: 'smooth' })
+//   })
+// }
+
+// allSection(servicesBtn, services)
+// allSection(faqBtn, faq)
+// allSection(contactsBtn, contacts)
+// }
+
+// smoothScroll()
+
+//header slider
+// const slider = () => {
+//   const slider = document.querySelector('.top-slider')
+//   console.log('slider: ', slider)
+
+
+// }
+
+// slider()
+
+//Service Carousel
+const serviceCarousel = () => {
+  const body = document.querySelector('body')
+  const modalCallBack = document.querySelector('.modal-callback')
+  const modalOverlay = document.querySelector('.modal-overlay')
+  const arrowLeft = document.querySelector('.arrow-left')
+  const arrowRigth = document.querySelector('.arrow-right')
+  const slidesToShow = 1
+  const slides = document.querySelectorAll('.services-carousel')
+  const options = {
+    position: 0,
+    infinity: false,
+    widthSlide: Math.floor(100 / slidesToShow),
+    maxPosition: slides.length - slidesToShow
   }
 
-  allSection(servicesBtn[0], services)
-  allSection(faqBtn[0], faq)
-  allSection(contactsBtn[0], contacts)
+
+  //let currentSlide = 0;
+
+  body.addEventListener('click', (event) => {
+    const target = event.target
+
+    if (target.closest('.fancyboxModal')) {
+      modalCallBack.style.display = 'block'
+      modalOverlay.style.display = 'block'
+    }
+
+    // if (target.closest('.arrow-left')) {
+    //   currentSlide++
+    // } else if (target.closest('.arrow-right')) {
+    //   currentSlide--
+    // }
+  })
+
+  //карусель
+
+  const prevSlider = () => {
+
+  }
+
+  const nextSlider = () => {
+    
+  }
+
+
+  arrowLeft.addEventListener('click', prevSlider)
+  arrowRigth.addEventListener('click', nextSlider)
+
 }
 
-smoothScroll()
+serviceCarousel()
+
+
+//Button services modal
+const buttonServicesModal = () => {
+
+  const body = document.querySelector('body')
+  const modalCallBack = document.querySelector('.modal-callback')
+  const modalOverlay = document.querySelector('.modal-overlay')
+
+  body.addEventListener('click', (event) => {
+    const target = event.target
+
+    if (target.closest('.button-services')) {
+      modalCallBack.style.display = 'block'
+      modalOverlay.style.display = 'block'
+    }
+  })
+}
+
+buttonServicesModal()
