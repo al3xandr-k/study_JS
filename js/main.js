8705ec48be97ab744693aeaa4c8fb3f8f5cfe020
Alexander Kupsha <red2@mail.ru>
2021-04-20 15:05:18 +0300

    diplomWork

--- js/main.js
+++ js/main.js
@@ -26,6 +26,41 @@ const modalCallBackForm = () => {
 
 modalCallBackForm()
 
+//Header Slider top-slider
+
+const topSliderScroll = () => {
+
+  const slide = document.querySelectorAll('.table')
+  const slideItem = document.querySelectorAll('.item')
+
+  let currentSlide = 0
+
+  const autoPlaySlide = () => {
+    slide[currentSlide].classList.remove('active')
+    slideItem[currentSlide].style.display = 'none'
+    currentSlide++
+
+    if (currentSlide >= slide.length) {
+      currentSlide = 0
+    }
+
+    slideItem[currentSlide].style.display = 'block'
+    slide[currentSlide].classList.add('active')
+  }
+
+  const startSlide = () => {
+    setInterval(autoPlaySlide, 3000)
+  }
+
+  // const stopSlide = () => {
+
+  // }
+
+  startSlide()
+}
+
+topSliderScroll()
+
 // const smoothScroll = () => {
 
 //   //btn top menu
@@ -118,7 +153,7 @@ const serviceCarousel = () => {
   }
 
   const nextSlider = () => {
-    
+
   }
 
 
