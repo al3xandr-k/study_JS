95c8e3453d3e0544286ac467fde673f5aca9eabb
Alexander Kupsha <red2@mail.ru>
2021-04-23 10:18:38 +0300

    finished

--- src/modules/checkData.js
+++ src/modules/checkData.js
@@ -1,32 +1,32 @@
 const checkData = () => {
-  const form = document.querySelectorAll("form"),
-    formBtn = document.querySelector(".feedback");
+  const form = document.querySelectorAll('form')
+  const formBtn = document.querySelector('.feedback')
   form.forEach((item) => {
-    item.addEventListener("focusout", (event) => {
-      const target = event.target;
-      if (target.placeholder === "Ваше имя") {
+    item.addEventListener('focusout', (event) => {
+      const target = event.target
+      if (target.placeholder === 'Ваше имя') {
         target.value = target.value
           .trim()
-          .replace(/[^\s\da-zа-я]/gi, "")
-          .replace(/\s+/g, " ");
-        if (target.value !== "") {
+          .replace(/[^\s\da-zа-я]/gi, '')
+          .replace(/\s+/g, ' ')
+        if (target.value !== '') {
           target.value = target.value
-            .split(" ")
+            .split(' ')
             .map(function (word) {
-              return word[0].toUpperCase() + word.substr(1);
+              return word[0].toUpperCase() + word.substr(1)
             })
-            .join(" ");
+            .join(' ')
         }
         if (target.value.search(/[^A-Za-z]/)) {
-          target.value = "";
+          target.value = ''
         }
-      } else if (target.placeholder === "Телефон*") {
+      } else if (target.placeholder === 'Телефон*') {
         if (target.value.length !== 12) {
-          target.style.border = "2px solid red";
-          formBtn.disabled = true;
+          target.style.border = '2px solid red'
+          formBtn.disabled = true
         }
       }
-    });
-  });
-};
-export default checkData;
+    })
+  })
+}
+export default checkData
--- src/modules/maskPhone.js
+++ src/modules/maskPhone.js
@@ -1,42 +1,43 @@
-function maskPhone(selector, masked = "+7__________") {
-  const elems = document.querySelectorAll(selector);
+function maskPhone(selector, masked = '+7__________') {
+  const elems = document.querySelectorAll(selector)
 
   function mask(event) {
-    const keyCode = event.keyCode;
-    const template = masked,
-      def = template.replace(/\D/g, ""),
-      val = this.value.replace(/\D/g, "");
-    let i = 0,
-      newValue = template.replace(/[_\d]/g, function (a) {
-        return i < val.length ? val.charAt(i++) || def.charAt(i) : a;
-      });
-    i = newValue.indexOf("_");
+    const keyCode = event.keyCode
+    const template = masked
+    const def = template.replace(/\D/g, '')
+    const val = this.value.replace(/\D/g, '')
+
+    let i = 0
+    let newValue = template.replace(/[_\d]/g, function (a) {
+        return i < val.length ? val.charAt(i++) || def.charAt(i) : a
+      })
+    i = newValue.indexOf('_')
     if (i !== -1) {
-      newValue = newValue.slice(0, i);
+      newValue = newValue.slice(0, i)
     }
     let reg = template
       .substr(0, this.value.length)
       .replace(/_+/g, function (a) {
-        return "\\d{1," + a.length + "}";
+        return '\\d{1,' + a.length + '}'
       })
-      .replace(/[+()]/g, "\\$&");
-    reg = new RegExp("^" + reg + "$");
+      .replace(/[+()]/g, '\\$&')
+    reg = new RegExp('^' + reg + '$')
     if (
       !reg.test(this.value) ||
       this.value.length < 5 ||
       (keyCode > 47 && keyCode < 58)
     ) {
-      this.value = newValue;
+      this.value = newValue
     }
-    if (event.type === "blur" && this.value.length < 5) {
-      this.value = "";
+    if (event.type === 'blur' && this.value.length < 5) {
+      this.value = ''
     }
   }
 
   for (const elem of elems) {
-    elem.addEventListener("input", mask);
-    elem.addEventListener("focus", mask);
-    elem.addEventListener("blur", mask);
+    elem.addEventListener('input', mask)
+    elem.addEventListener('focus', mask)
+    elem.addEventListener('blur', mask)
   }
 }
-export default maskPhone;
+export default maskPhone
